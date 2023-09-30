bbc404ddfdd9039ab3617ee101313b05f36f7c78
Ibrahim Aslan <i.aslan2020@gtu.edu.tr>
2023-09-30 17:06:18 +0300

    [Runtime] Add Singleton & Debugger

---
+++ iboshEngine/Assets/Runtime/IboshDebug/IboshDebugger.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+namespace IboshEngine.IboshDebug
+{
+    public static class IboshDebugger
+    {
+        public enum DebugColor
+        {
+            Black,
+            Blue,
+            Cyan,
+            Gray,
+            Green,
+            Grey,
+            Magenta,
+            Red,
+            White,
+            Yellow
+        }
+
+        public static void LogMessage(string message)
+        {
+            if (!Debug.isDebugBuild) return;
+
+            Debug.Log(message);
+        }
+
+        public static void LogMessage(string message, DebugColor messageColor)
+        {
+            if (!Debug.isDebugBuild) return;
+
+            Debug.Log($"<color={DebugColorValue(messageColor)}> {message}</color>");
+        }
+
+        public static void LogMessage(string message, string header, DebugColor messageColor, DebugColor headerColor)
+        {
+            if (!Debug.isDebugBuild) return;
+
+            Debug.Log($"<color={DebugColorValue(headerColor)}> [{header}] </color> <color={DebugColorValue(messageColor)}> {message}</color>");
+        }
+
+        public static void LogMessage(string[] messages, string header, DebugColor messageColor, DebugColor headerColor)
+        {
+            if (!Debug.isDebugBuild) return;
+
+            Debug.Log($"<color={DebugColorValue(headerColor)}> [{header}] </color> <color={DebugColorValue(messageColor)}> {string.Join(",", messages)}</color>");
+        }
+
+        public static void LogError(string error, string header, DebugColor messageColor, DebugColor headerColor)
+        {
+            Debug.LogError($"<color={DebugColorValue(headerColor)}> [{header}] </color> <color={DebugColorValue(messageColor)}> {error}</color>");
+        }
+
+        public static void LogWarning(string error, string header, DebugColor messageColor, DebugColor headerColor)
+        {
+            if (!Debug.isDebugBuild) return;
+
+            Debug.LogWarning($"<color={DebugColorValue(headerColor)}> [{header}] </color> <color={DebugColorValue(messageColor)}> {error}</color>");
+        }
+
+        private static string DebugColorValue(DebugColor colorEnum)
+        {
+            return colorEnum switch
+            {
+                DebugColor.Black => "black",
+                DebugColor.Blue => "blue",
+                DebugColor.Cyan => "cyan",
+                DebugColor.Gray => "gray",
+                DebugColor.Green => "green",
+                DebugColor.Grey => "grey",
+                DebugColor.Magenta => "magenta",
+                DebugColor.Red => "red",
+                DebugColor.White => "white",
+                DebugColor.Yellow => "yellow",
+
+                _ => default
+            };
+        }
+    }
+
+}
---
+++ iboshEngine/Assets/Runtime/IboshSingleton/IboshSingleton.cs
@@ -0,0 +1,26 @@
+using IboshEngine.IboshDebug;
+using UnityEngine;
+
+namespace IboshEngine.Singleton
+{
+    [DefaultExecutionOrder(-5)]
+    public class IboshSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+
+        public virtual void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this as T;
+            }
+            else
+            {
+                IboshDebugger.LogError($"There is an instance of the {typeof(T).FullName}", "IboshSingleton", IboshDebugger.DebugColor.Red, IboshDebugger.DebugColor.Black);
+
+                if (Instance.gameObject) Destroy(Instance.gameObject);
+                Instance = this as T;
+            }
+        }
+    }
+}
